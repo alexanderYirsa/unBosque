243c85585baa3ffafb35a8653b340abc87bfc867
AlexYirsa <vyirsa@bancodebogota.com.co>
2021-09-21 22:52:05 -0500

    Creación proyecto

---
+++ src/test/java/prueba/TConexion.java
@@ -0,0 +1,13 @@
+package prueba;
+
+import conexion.Conexion;
+
+public class TConexion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conexion con = new Conexion();
+		con.conectar();
+	}
+
+}
